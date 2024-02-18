f68f338362fae9e1eba43ed522f24f6e33e23015
LozanoJohan <lozanojohan321@gmail.com>
2024-02-18 10:27:30 -0500

    dropfile

--- src/components/MarketModelCard.jsx
+++ src/components/MarketModelCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { DropFile } from "./dropFile"
+import { DropFile } from "./Dropfile";
 
 export const MarketModelCard = ({ className, contract, address }) => {
 
