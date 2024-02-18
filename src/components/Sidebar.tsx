27a6a00af32cecf5d9b4f1075bc1c449af07c9b6
sebastian2000pk <juochoas@unal.edu.co>
2024-02-18 15:48:30 -0500

    [add] toogle navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
 
                     <ul className="space-y-2 font-medium">
                         <li>
-                            <Link href="/" className="flex items-center p-2  rounded-lg text-white  hover:bg-gray-700 group gap-4">
+                            <Link href="/" className="flex items-center p-2  rounded-lg text-white  hover:bg-gray-700 group gap-4" onClick={toogleOpen}>
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 text-gray-400 group-hover: group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                                     <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                                 </svg>
@@ -48,7 +48,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
                     </ul>
                     <ul className="space-y-2 font-medium">
                         <li>
-                            <Link href="/new-request-data" className="flex items-center p-2  rounded-lg text-white  hover:bg-gray-700 group gap-4">
+                            <Link href="/new-request-data" onClick={toogleOpen} className="flex items-center p-2  rounded-lg text-white  hover:bg-gray-700 group gap-4">
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 text-gray-400 group-hover: group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                                     <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                                 </svg>
@@ -56,7 +56,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
                             </Link>
                         </li>
                         <li>
-                            <Link href="/my-projects" className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
+                            <Link href="/my-projects" onClick={toogleOpen} className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 dark:text-gray-400 group-hover: dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                                     <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                                 </svg>
@@ -70,7 +70,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
                     <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
 
                         <li>
-                            <Link href="/ViewModels" className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
+                            <Link href="/ViewModels" onClick={toogleOpen} className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 dark:text-gray-400 group-hover: dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
                                     <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
                                 </svg>
@@ -78,7 +78,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
                             </Link>
                         </li>
                         <li>
-                            <Link href="/UploadModel" className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
+                            <Link href="/UploadModel" onClick={toogleOpen} className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 dark:text-gray-400 group-hover: dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
                                     <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
                                 </svg>
@@ -86,7 +86,7 @@ export const Sidebar: FC<SidebarProps> = ({ children }) => {
                             </Link>
                         </li>
                         <li>
-                            <Link href="/MyModels" className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
+                            <Link href="/MyModels" onClick={toogleOpen} className="flex items-center p-2  rounded-lg dark:text-white  dark:hover:bg-gray-700 group gap-4">
                                 <svg className="flex-shrink-0 w-5 h-5  transition duration-75 dark:text-gray-400 group-hover: dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
                                     <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
                                 </svg>
