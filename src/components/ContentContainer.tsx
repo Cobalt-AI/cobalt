6881ff512d36235600f3cb245513bedfc07cb92e
LozanoJohan <lozanojohan321@gmail.com>
2024-02-18 09:35:15 -0500

    bg

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const ContentContainer: React.FC<Props> = ({ children }) => {
 
   return (
-    <div className="flex-1 flex  flex-col justify-between items-center bg-slate-900 pt-16">
+    <div className="flex-1 flex  flex-col justify-between items-center bg-[#F1F1F1] pt-16">
       <div className='max-w-lg w-full'>
         <div className="items-center drawer-content flex flex-col justify-between w-full">
           {children}
